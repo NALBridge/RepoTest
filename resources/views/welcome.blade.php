4900b00a4e6afb63f490ca923fbbcbdc68dd7b3c
Trinh Tuan Thuy <thuytt@nal.vn>
2017-08-16 17:55:27 +0700

    #2[mod] modifying

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel is my favorite!
                 </div>
 
                 <div class="links">
