cba4e63ea0f251d01cc3156cab06d2cb2eb346b8
Trinh Tuan Thuy <thuytt@nal.vn>
2017-08-17 08:24:00 +0700

    #3[mod] this is modyfying 7

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
                 <div class="title m-b-md">
                     Laravel is my favorite framework!
                     This is awesome!gsgs
-                    fadfdafdsssefsgrfafaeefafaefddsdsfdsfsdfsd
+                    fadfdafdsssefsgrfafaeefafaefddsdsfdsfsdfsddsdfsdfsdf
                 </div>
 
                 <div class="links">
